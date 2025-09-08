593ef2479e116acb82bdba818311c5fb1b2e3936
Israel Nunes <israelnetonunes@gmail.com>
2025-09-08 09:26:17 -0300

    refact: add directories for header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import HeroSection from "@/app/components/hero-section";
+import HeroSection from "@/app/components/Header/hero-section";
 import LogoCloud from "@/app/components/logo-cloud";
-import Footer from "@/app/components/footer";
+import Footer from "@/app/components/Footer/footer";
 import Features from "./components/features";
 
 
