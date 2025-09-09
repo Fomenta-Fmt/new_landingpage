6dd44824cad4b198ecc536f700cf180cc914d3ea
Israel Nunes <israelnetonunes@gmail.com>
2025-09-09 16:51:03 -0300

    refact: update metadata - for better SEO practices

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import { Metadata } from "next";
 import HeroSection from "@/app/components/Header/hero-section";
 import LogoCloud from "@/app/components/logo-cloud";
 import Footer from "@/app/components/Footer/footer";
 import Features from "@/app/components/features";
 import Solution from "@/app/components/solution";
 
+export const metadata: Metadata = {
+  title: "Fomenta - Impulsione o financiamento dos seus projetos",
+  description: "A Fomenta conecta você aos melhores editais e oportunidades de fomento. Encontre o edital perfeito para o seu projeto e acelere seu crescimento.",
+  keywords: ['editais', 'fomento', 'startup', 'inovação', 'captação de recursos', 'pesquisa', 'desenvolvimento científico']
+};
 
 export default function Home() {
   return (
