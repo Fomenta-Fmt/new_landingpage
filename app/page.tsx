6935a978465840b8619b9d77f4cca60fa98478de
Israel Nunes <israelnetonunes@gmail.com>
2025-09-09 16:51:24 -0300

    refact: update metadata - for better SEO practices

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,15 @@ import Solution from "@/app/components/solution";
 export const metadata: Metadata = {
   title: "Fomenta - Impulsione o financiamento dos seus projetos",
   description: "A Fomenta conecta você aos melhores editais e oportunidades de fomento. Encontre o edital perfeito para o seu projeto e acelere seu crescimento.",
-  keywords: ['editais', 'fomento', 'startup', 'inovação', 'captação de recursos', 'pesquisa', 'desenvolvimento científico']
+  keywords: ['editais', 'fomento', 'startup', 'inovação', 'captação de recursos', 'pesquisa', 'desenvolvimento científico'],
+  openGraph: {
+    title: "Fomenta - Impulsione o financiamento dos seus projetos",
+    description: "A Fomenta conecta você aos melhores editais e oportunidades de fomento. Encontre o edital perfeito para o seu projeto e acelere seu crescimento.",
+    url: "https://www.fomenta.com.br",
+    siteName: "Fomenta",
+    locale: "pt-BR",
+    type: "website"
+  }
 };
 
 export default function Home() {
