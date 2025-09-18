4e4bf3192ec92365ea859e6d1ae0ba265ca08e21
Israel Nunes <israelnetonunes@gmail.com>
2025-09-18 14:43:49 -0300

    refact: atualizar texto na seção who

--- app/components/who.tsx
+++ app/components/who.tsx
@@ -9,11 +9,16 @@ export default function WhoSection() {
                     <span className="text-caption -ml-6 -mt-3.5 block w-max bg-background px-6 dark:bg-gray-950">Quem somos</span>
                     <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
                         <div className="sm:w-2/5">
-                            <h2 className="text-3xl font-bold sm:text-4xl">Conectamos pesquisadores ao fomento</h2>
+                            <h2 className="text-3xl font-bold sm:text-4xl">Conectando pesquisadores ao fomento</h2>
                         </div>
                         <div className="mt-6 sm:mt-0">
-                            <p className='mb-3'>Somos uma solução desenvolvida para <span className='text-secondary'>fortalecer</span> a produção científica, <span className='text-secondary'>potencializar</span> projetos e <span className='text-secondary'>conectar</span> talentos.</p>
-                            <p>Buscamos crescimento constante, por isso, fazemos parte do Programa Nascer 2025</p>
+                            <p className='mb-3'>
+                                O Fomenta é uma <span className='text-secondary'>iniciativa que une tecnologia, ciência e inovação</span> para apoiar o desenvolvimento de projetos e a conexão entre talentos. 
+                                Nossa dedicação está em entregar <span className='text-secondary'>soluções úteis</span> e <span className='text-secondary'>recomendações precisas</span> para quem busca avançar na pesquisa.
+                            </p>
+                            <p>
+                                A cada etapa, expandimos nossas possibilidades e fortalecemos nosso compromisso com a qualidade. Estar no Programa Nascer 2025 é reflexo dessa visão de futuro
+                            </p>
                             <Image
                                 className="h-52 w-fit -top-12 relative"
                                 src="/programa-nascer.svg"
